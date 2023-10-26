4484d3013f4c4e454f1b1022fbd3e7c42eea3901
Julia Horton <juliahorton00@gmail.com>
2023-10-26 01:03:07 -0500

    complete exercise files

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 // Car Object Orientation Challenge // 
 
-// Part One: Create a class for vehicle. Each vehicle instance should have properties for make, model, and year. Each vehicle instance should have access to a method called "honk", which returns the string "beep". Each vehicle instance should also have a method called toString, which returns the string containing the make, model, and year. 
+// Part One: Create a class for vehicle. Each vehicle instance should have properties for make, model, and year. Each vehicle instance should have access to a method called "honk", which returns the string "Beep". Each vehicle instance should also have a method called toString, which returns the string containing the make, model, and year. 
 
 // let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
 // myFirstVehicle.honk(); // "Beep."
 // myFirstVehicle.toString(); // "The vehicle is a Honda Monster Truck from 1999."
 
-
-
-
+class Vehicle {
+    constructor(make,model,year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    honk() {
+        return "Beep";
+    }
+    toString() {
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+    }
+}
 
 
 
@@ -19,17 +29,32 @@
 // myFirstCar.honk();     // "Beep."
 // myFirstCar.numWheels;  // 4
 
-
-
-
+class Car extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 4;
+    }
+}
 
 
 
 // Part Three: Create a class for a motorcycle. The "Motorcycle" class should inherit from Vehicle and each motorcycle instance should have a property called numWheels which has a value of 2. It should also have a revEngine method which returns "VROOM!!!"
 
+// let myFirstMotorcycle = new Motorcycle("Honda", "Nighthawk", 2000);
+// myFirstMotorcycle.toString(); // "The vehicle is a Honda Nighthawk from 2000."
+// myFirstMotorcycle.honk();     // "Beep."
+// myFirstMotorcycle.revEngine(); // "VROOM!!!"
+// myFirstMotorcycle.numWheels;  // 2
 
-
-
+class Motorcycle extends Vehicle {
+    constructor (make, model, year) {
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+    revEngine() {
+        return "VROOM!!!";
+    }
+}
 
 
 
@@ -49,3 +74,19 @@
 
 // garage.add(new Motorcycle("Honda", "Nighthawk", 2001));
 // // "Sorry, we're full."
+
+class Garage {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.vehicles = [];
+    }
+    add(v) {
+        if (v instanceof Vehicle === false){
+            return "Only vehicles are allowed in here!"
+        } else if (this.vehicles.length < this.capacity) {
+            this.vehicles.push(v);
+            return "Vehicle added!"
+        }
+        return "Sorry, we're full."
+    }
+}
